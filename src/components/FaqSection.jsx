bc22d0a7077371a70c0d3452e794397e5f7744fc
Ksenia Baranova <baranova-ksenia@mail.ru>
2021-08-04 15:58:23 +0300

    faq section

---
+++ src/components/FaqSection.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+export default function FaqSection() {
+  return (
+    <div className="faq">
+      <h2>
+        Any Question <span>FAQ</span>
+      </h2>
+      <div className="question">
+        <h4>How do I start? </h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+      </div>
+
+      <div className="question">
+        <h4>Daily Schedule </h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+      </div>
+
+      <div className="question">
+        <h4>Diferent Payment Methods </h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+      </div>
+
+      <div className="question">
+        <h4>What Products do you offer.</h4>
+        <div className="answer">
+          <p>Lorem ipsum dolor sit amet.</p>
+          <p>Lorem ipsum dolor sit amet.</p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,10 +1,14 @@
 import React from "react";
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 
 export default function AboutPage() {
   return (
     <div>
       <AboutSection />
+      <ServicesSection />
+      <FaqSection />
     </div>
   );
 }
